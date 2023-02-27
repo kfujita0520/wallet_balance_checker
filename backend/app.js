f7330ab804613f618d4ce915c8b55e871f865deb
FUJITA KAZUHISA <>
2023-02-28 02:12:48 +0900

    backend initial

---
+++ backend/app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var MemoryStore = require('memorystore')(session);
+const uuidv4 = require('uuid').v4;
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+const port = 4000;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//session configuration must come before routing
+const sessionStore = new MemoryStore({
+  checkPeriod: 86400000 // prune expired entries every 24h
+});
+app.use(session({
+  genid: (req) => {
+      return uuidv4(); // Generate a unique ID for each session
+    },
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore
+}));
+
+app.use('/', indexRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, function(){
+  console.log('Example app listening on port ' + port);
+});
+
+module.exports = app;
---
+++ backend/routes/index.js
@@ -0,0 +1,234 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/getRates', async function(req, res, next) {
+
+    console.log("session id: ", req.session.id);
+    console.log("initialized: ", req.session.initialized);
+    const sessionIdFromCookie = req.cookies['connect.sid'];
+    console.log("sessionId from Cookie: ", sessionIdFromCookie);
+
+    if(req.session.initialized===undefined){
+        console.log("set up the rate");
+        req.session.initialized = true;
+        let rates = await initiateRate();
+        req.session.usd = rates.usd;
+        req.session.eur = rates.eur;
+        res.json({
+            'usd' : req.session.usd,
+            'eur' : req.session.eur,
+            'initialized' : false
+        });
+        return;
+    }
+
+    let usd = req.session.usd;
+    let eur = req.session.eur;
+    console.log('usd: ', usd);
+    console.log('eur: ', eur);
+    res.json({
+        'usd' : usd,
+        'eur' : eur,
+        'initialized' : true
+    });
+
+});
+
+async function initiateRate(){
+
+    let usd, eur;
+    await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+        .then(response => {
+            usd = response.data.ethereum.usd;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    // Fetch ETH/EUR price from API
+    await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=eur')
+        .then(response => {
+            eur = response.data.ethereum.eur;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    return {
+        "usd": usd,
+        "eur": eur
+    }
+
+}
+
+router.get('/updateRate', async function(req, res, next) {
+
+    const currency = req.query.currency;
+    const rate = req.query.rate;
+    let result = false;
+
+    if(!(["USD", "EUR"].includes(currency)) ){
+        let response = {
+            'status': 0,
+            'msg': "currency is not supported: "+currency
+        }
+        return res.json(response);
+    } else if (rate === undefined) { //TODO format check is needed
+        let response = {
+            'status': 0,
+            'msg': "rate is missing"
+        }
+        return res.json(response);
+    }
+
+    if (req.cookies['connect.sid'] === undefined || req.session.initialized !== true){
+        //finish with error message. no cookies information is sent
+        return　res.status(500).json({
+            status: 0,
+            msg: "cookie and session id is not initialized properly"
+        });
+    }
+
+    if (currency === "USD"){
+        req.session.usd = rate;
+    } else if(currency === "EUR") {
+        req.session.eur = rate;
+    }
+
+    res.json({
+        'status' : 1,
+        'usd' : req.session.usd,
+        'eur' : req.session.eur,
+        'initialized' : true
+    });
+
+
+});
+
+router.get('/getBalance', async function(req, res, next) {
+
+    let currency = req.query.currency;
+    const address = req.query.address;
+    let rate = 0;
+    let ethAmount = 0;
+    let currencyAmount = 0;
+    if(!(["USD", "EUR"].includes(currency)) ){
+        let response = {
+            'status': 0,
+            'msg': "currency is not supported: "+currency
+        }
+        return res.json(response);
+    } else if (address === undefined) {//TODO address format check is needed
+        let response = {
+            'status': 0,
+            'msg': "address is missing"
+        }
+        return res.json(response);
+    }
+
+    if (req.cookies['connect.sid'] === undefined || req.session.initialized !== true){
+        req.session.initialized = true;
+        let rates = await initiateRate();
+        req.session.usd = rates.usd;
+        req.session.eur = rates.eur;
+
+    }
+
+    if (currency === "USD"){
+        rate = req.session.usd;
+    } else if(currency === "EUR") {
+        rate = req.session.eur;
+    }
+
+    await axios.get(`https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=YourApiKeyToken`)
+        .then(response => {
+            // Convert balance from wei to ETH
+            ethAmount = Number(response.data.result) / 10 ** 18;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+    currencyAmount = (ethAmount * rate).toFixed(2);
+
+    let response = {
+        'status': 1,
+        'ethAmount': ethAmount,
+        'currencyAmount': currencyAmount
+    }
+    return res.json(response);
+
+
+});
+
+
+//return true only if wallet has old transaction. Other case including input error will return false.
+router.get('/checkOldWallet', async function(req, res, next) {
+
+    const walletAddress = req.query.address;
+    let result = false;
+    let status = 0;
+    console.log(walletAddress);
+    if(walletAddress === undefined){ //TODO address format check is needed
+        let response = {
+            'status': 0,
+            'result': result,
+            'msg': 'Address is missing'
+        }
+        return res.json(response);
+    }
+
+    let txlist = await axios.get(`https://api.etherscan.io/api?module=account&action=txlist&address=${walletAddress}&startblock=0&page=1&offset=3&sort=desc&APIKey=NSZCD6S4TKVWRS13PMQFMVTNP6H7NAGHUY`)
+    console.log('response.data.status', txlist.data.status);
+    if(txlist.data.status === "0"){
+        //TODO complete new account address having 0 transaction also categorized here and considered as wrong input.
+        console.log(txlist.data);
+        let response = {
+            'status': 0,
+            'result': result,
+            'msg': 'No Transaction is found'
+        }
+        return res.json(response);
+    }
+    console.log('response.data.result[0] ', txlist.data.result[0]);
+    result = isOverOneYear(txlist.data.result[0].timeStamp);
+    console.log(`Log result:  ${result}`)
+
+    let response = {
+        'status': 1,
+        'result': result,
+        'msg': 'Success'
+    }
+    res.json(response);
+
+
+});
+
+
+function isOverOneYear(timestamp) {
+    // Get the current timestamp
+    const now = Date.now() /1000;
+
+    // Get the timestamp for one year ago
+    const oneYearAgo = now - (365 * 24 * 60 * 60);
+    console.log(timestamp);
+    console.log(oneYearAgo);
+
+    // Compare the given timestamp to the one year ago timestamp
+    if (timestamp >= oneYearAgo && timestamp <= now) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+module.exports = router;
